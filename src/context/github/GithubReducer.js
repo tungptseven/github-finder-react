4bb5ed6b905558935532e52d4505769a77ba984c
Phan Tung <tungpt.seven@gmail.com>
2022-08-09 21:25:18 +0700

    Initial

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,37 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+        isLoading: false
+      }
+    case 'GET_USER':
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      }
+    case 'CLEAR_USERS':
+      return {
+        ...state,
+        users: [],
+        isLoading: false
+      }
+    case 'GET_REPOS':
+      return {
+        ...state,
+        repos: action.payload,
+        isLoading: false,
+      }
+    case 'SET_LOADING':
+      return {
+        ...state,
+        isLoading: true
+      }
+    default:
+      return state
+  }
+}
+
+export default githubReducer
